3093a981acd8322733cb959a8e9cefb09cb65b87
yangxingxing1024 <1154433461@qq.com>
2019-11-21 22:28:04 +0800

    123

---
+++ src/main/java/com/dy/xdy.java
@@ -0,0 +1,31 @@
+package src.main.java.com.dy;
+
+public class xdy {
+    private String username;
+    private Integer id;
+    private String bb;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBb() {
+        return bb;
+    }
+
+    public void setBb(String bb) {
+        this.bb = bb;
+    }
+}
